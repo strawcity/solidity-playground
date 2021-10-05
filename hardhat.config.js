709eb98610d2f5bad638637542b6b3781183e0ef
Erik Nilsson <erik@strawcitydesign.com>
2021-10-05 15:47:04 +0200

    Adding dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.REACT_APP_ALCHEMY_STAGING_URL,
+      accounts: [
+        process.env.REACT_APP_ALCHEMY_PRIVATE_KEY,
+      ],
+    },
+  },
 };
